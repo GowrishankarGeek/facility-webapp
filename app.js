e43de9d5557af53e7a6cff799137db313c62980b
Kiaresch Mussawisade <kiaresch.mussawisade@cologne-intelligence.de>
2014-05-09 15:55:30 +0200

    hello world

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 var express = require('express')
 , app = express()
-, cons = require('consolidate')
-, routes = require('./routes');
+, cons = require('consolidate');
 
 
 app.engine('html', cons.swig);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
-app.get("/", function(req,res))
-routes(app);
+app.get("/", function(req,res) {
+  res.send("hello world");
+});
 
 app.listen(8082);
 console.log('Express server listening on port 8082');
